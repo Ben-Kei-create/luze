66e506618fbd4e9ddc35f6991c0ad12882a1a694
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2026-08-12 12:15:54 +0900

    Initial Commit

---
+++ luze/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  luze
+//
+//  Created by 茂木史明 on 2026/08/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ luze/luzeApp.swift
@@ -0,0 +1,17 @@
+//
+//  luzeApp.swift
+//  luze
+//
+//  Created by 茂木史明 on 2026/08/12.
+//
+
+import SwiftUI
+
+@main
+struct luzeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
